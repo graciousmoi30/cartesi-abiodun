81315c678f75d36fd5d4dfcc541dc34f04a8240d
graciousmoi30 <graciousmoi30@gmail.com>
2023-12-07 16:52:42 +0100

    script updated

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const token = await ethers.deployContract("DAOToken");
-
-  await token.waitForDeployment();
+  const DAOToken = await ethers.getContractFactory("DAOToken");
+  const token = await DAOToken.deploy();
+  await token.deployed();
 
   console.log(`The address is ${token.address}`);
+  // CONTRACT ADDRESS: 0xc4C8540576e682FE80d8515dfB59D592741DBac8
 }
 
 // We recommend this pattern to be able to use async/await everywhere
