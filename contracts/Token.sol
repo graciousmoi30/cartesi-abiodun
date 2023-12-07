86914c870f1a7702cf96aad14012cf2eeb8f183f
graciousmoi30 <graciousmoi30@gmail.com>
2023-12-07 16:33:24 +0100

    second commit

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DAOToken is ERC1155, Ownable {
+// contract DAOToken is ERC1155, Ownable {
+contract DAOToken is ERC1155 {
     uint256 public constant MAIN_TOKEN_ID = 1;
     
     // Mapping to track the owner of the soulbound main token
-    mapping(address => bool) private soulboundOwners;
+    mapping(address => bool) public soulboundOwners;
 
     // Modifier to check if the sender owns the soulbound main token
     modifier onlySoulboundOwner() {
@@ -23,7 +24,7 @@ contract DAOToken is ERC1155, Ownable {
     }
 
     // Function to mint item tokens, only callable by the contract owner
-    function mintItem(address account, uint256 id, uint256 amount, bytes memory data) external onlyOwner {
+    function mintItem(address account, uint256 id, uint256 amount, bytes memory data) external onlySoulboundOwner {
         _mint(account, id, amount, data);
     }
 
@@ -38,16 +39,8 @@ contract DAOToken is ERC1155, Ownable {
     }
 
     // Function to set a new soulbound main token owner, only callable by the contract owner
-    function setSoulboundOwner(address account) external onlyOwner {
+    function setSoulboundOwner(address account) external onlySoulboundOwner {
         require(!soulboundOwners[account], "Address already owns the soulbound main token");
         soulboundOwners[account] = true;
-    }
-
-    // Override _beforeTokenTransfer to check ownership before transfers
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal override {
-        if (ids.length == 1 && ids[0] == MAIN_TOKEN_ID) {
-            require(soulboundOwners[from] && !soulboundOwners[to], "Main token is soulbound and cannot be transferred");
-        }
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
